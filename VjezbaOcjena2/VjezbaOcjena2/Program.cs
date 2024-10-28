daea6abf7547e5b9d02d34ddbc78b05fcba44bd7
LeonCicak <leoncicak07@gmail.com>
2024-10-28 13:09:32 +0100

    Add files via upload

---
+++ VjezbaOcjena2/VjezbaOcjena2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VjezbaOcjena2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite string:");
+            string s1 = Console.ReadLine();
+
+            if (s1.Length < 5)
+            {
+                Console.WriteLine(s1);
+            }
+            else
+            {
+                string result = s1.Substring(2, s1.Length - 4);
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
